b3aa2e1ff7f4ce81916ab623d6096f00e34a6e3b
Edrisse <edrisseasm@gmail.com>
2018-11-15 14:02:51 +0200

    Fix order frequency metadata deploy when order frequency exists and is in use

--- api-1.10/src/main/java/org/openmrs/module/metadatadeploy/handler/impl/OrderFrequencyDeployHandler.java
+++ api-1.10/src/main/java/org/openmrs/module/metadatadeploy/handler/impl/OrderFrequencyDeployHandler.java
@@ -11,23 +11,27 @@ import org.springframework.beans.factory.annotation.Qualifier;
 @OpenmrsProfile(openmrsVersion = "1.10.0")
 @Handler(supports = OrderFrequency.class)
 public class OrderFrequencyDeployHandler extends AbstractObjectDeployHandler<OrderFrequency> {
-
-    @Autowired
-    @Qualifier("orderService")
-    private OrderService orderService;
-
-    @Override
-    public OrderFrequency fetch(String uuid) {
-        return orderService.getOrderFrequencyByUuid(uuid);
-    }
-
-    @Override
-    public OrderFrequency save(OrderFrequency obj) {
-        return orderService.saveOrderFrequency(obj);
-    }
-
-    @Override
-    public void uninstall(OrderFrequency obj, String reason) {
-        orderService.retireOrderFrequency(obj, reason);
-    }
+	
+	@Autowired
+	@Qualifier("orderService")
+	private OrderService orderService;
+	
+	@Override
+	public OrderFrequency fetch(String uuid) {
+		return orderService.getOrderFrequencyByUuid(uuid);
+	}
+	
+	@Override
+	public OrderFrequency save(OrderFrequency obj) {
+		OrderFrequency existing = fetch(obj.getUuid());
+		if (existing == null) {
+			return orderService.saveOrderFrequency(obj);
+		}
+		return obj;
+	}
+	
+	@Override
+	public void uninstall(OrderFrequency obj, String reason) {
+		orderService.retireOrderFrequency(obj, reason);
+	}
 }
